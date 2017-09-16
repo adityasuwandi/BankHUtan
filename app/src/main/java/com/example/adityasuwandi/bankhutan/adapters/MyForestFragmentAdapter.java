5c730043fd960782fabe1c9c02040e90956efc9a
reshalfahsi <resha.alfahsi@gmail.com>
2017-09-17 00:41:31 +0700

    18/09/17 - 00.41

--- app/src/main/java/com/example/adityasuwandi/bankhutan/adapters/MyForestFragmentAdapter.java
+++ app/src/main/java/com/example/adityasuwandi/bankhutan/adapters/MyForestFragmentAdapter.java
@@ -1,83 +1,12 @@
 package com.example.adityasuwandi.bankhutan.adapters;
 
-import android.content.res.Resources;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.util.SparseArray;
-import android.view.ViewGroup;
 
-import com.example.adityasuwandi.bankhutan.fragments.HarvestFragment;
-import com.example.adityasuwandi.bankhutan.fragments.InProgressFragment;
-import com.example.adityasuwandi.bankhutan.fragments.NewTreeFragment;
 
 /**
  * Created by root on 15/09/17.
  */
 
-public class MyForestFragmentAdapter extends FragmentPagerAdapter {
+public class MyForestFragmentAdapter {
 
-    String[] title = new String[]{
-            "New Tree", "In Progress", "Harvest"
-    };
 
-    private final Resources resources;
-
-    SparseArray<Fragment> registeredFragments = new SparseArray<>();
-
-    public MyForestFragmentAdapter(Resources resources,FragmentManager fm) {
-        super(fm);
-        this.resources = resources;
-    }
-
-    @Override
-    public Object instantiateItem(ViewGroup container, int position) {
-        Fragment fragment = (Fragment) super.instantiateItem(container, position);
-        registeredFragments.put(position, fragment);
-        return fragment;
-    }
-
-    //method ini yang akan memanipulasi penampilan Fragment dilayar
-    @Override
-    public Fragment getItem(int position) {
-        Fragment fragment;
-        switch (position){
-            case 0:
-                fragment = new NewTreeFragment();
-                break;
-            case 1:
-                fragment = new InProgressFragment();
-                break;
-            case 2:
-                fragment = new HarvestFragment();
-                break;
-            default:
-                fragment = null;
-                break;
-        }
-
-        return fragment;
-    }
-
-    @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
-        registeredFragments.remove(position);
-        super.destroyItem(container, position, object);
-    }
-
-    public Fragment getRegisteredFragment(int position) {
-        return registeredFragments.get(position);
-    }
-
-    @Override
-    public CharSequence getPageTitle(int position) {
-
-//        return title[position];
-        return null;
-    }
-
-    @Override
-    public int getCount() {
-        return title.length;
-    }
 }
---
+++ app/src/main/java/com/example/adityasuwandi/bankhutan/fragments/CatalogFragment.java
@@ -0,0 +1,32 @@
+package com.example.adityasuwandi.bankhutan.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.adityasuwandi.bankhutan.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CatalogFragment extends Fragment {
+
+
+    public CatalogFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
